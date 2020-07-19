ec6b7c44ef7bbab9af9c6ecc9206eaff61b69dbe
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-07-19 21:10:16 +0900

    計算機能追加

--- Part2/ViewController.swift
+++ Part2/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField1: UITextField!
+    @IBOutlet var textField2: UITextField!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var answerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField1.becomeFirstResponder()
+        answerLabel.adjustsFontSizeToFitWidth = true
     }
-
-
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func showTheAnswer(_ sender: Any) {
+        let doubleNum1 = Double(textField1.text ?? "") ?? 0
+        let doubleNum2 = Double(textField2.text ?? "") ?? 0
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            answerLabel.text = String(doubleNum1 + doubleNum2)
+        case 1:
+            answerLabel.text = String(doubleNum1 - doubleNum2)
+        case 2:
+            answerLabel.text = String(doubleNum1 * doubleNum2)
+        case 3:
+            if doubleNum2 == 0 {
+                answerLabel.text = "割る数には0以外を入力してください"
+            } else {
+                let answer = floor(doubleNum1 / doubleNum2 * 100)
+                answerLabel.text = String(answer / 100)
+            }
+        default:
+            return
+        }
+    }
 }
 
