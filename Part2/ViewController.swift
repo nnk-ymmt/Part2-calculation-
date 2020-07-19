d370fadddbe8d8fb1b3c76624991a64182c56959
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-07-19 20:03:39 +0900

    Initial Commit

---
+++ Part2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Part2
+//
+//  Created by 山本ののか on 2020/07/19.
+//  Copyright © 2020 Nonoka Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
