735f6126c294fb00044d6c236b193cfaf0d82e25
nnk-ymmt <garden.n1234567890@gmail.com>
2020-11-27 01:53:37 +0900

    解答例追加、レイアウト調整

--- Part2/ViewController.swift
+++ Part2/ViewController.swift
@@ -8,27 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet private weak var textField1: UITextField!
     @IBOutlet private weak var textField2: UITextField!
     @IBOutlet private weak var segmentedControl: UISegmentedControl!
     @IBOutlet private weak var answerLabel: UILabel!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         textField1.becomeFirstResponder()
         answerLabel.adjustsFontSizeToFitWidth = true
     }
-    
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
-    
-    
+
     @IBAction private func showTheAnswer(_ sender: Any) {
         let doubleNum1 = Double(textField1.text ?? "") ?? 0
         let doubleNum2 = Double(textField2.text ?? "") ?? 0
@@ -67,75 +63,152 @@ class ViewController: UIViewController {
 // 方法1
 //switch segmentedControl.selectedSegmentIndex, doubleNum2 {
 //case (0, _):
-//    answerLabel.text = "\(intNum1 + intNum2)"
+//    answerLabel.text = "\(doubleNum1 + doubleNum2)"
 //case (1, _):
-//    answerLabel.text = "\(intNum1 - intNum2)"
+//    answerLabel.text = "\(doubleNum1 - doubleNum2)"
 //case (2, _):
-//    answerLabel.text = "\(intNum1 * intNum2)"
+//    answerLabel.text = "\(doubleNum1 * doubleNum2)"
 //case (3, 0):
 //    answerLabel.text = "割る数には0以外を入力してください"
 //case (3, _):
-//    answerLabel.text = "\(intNum1 / intNum2)"
-//    }
+//    answerLabel.text = "\(doubleNum1 / doubleNum2)"
 //default:
 //    fatalError("計算できません")
 //}
 
 // 方法2
 // これだとswitch文のdefaultが無くなる
-// enum Operation: Int {
-//     case addition
-//     case subtraction
-//     case multiplication
-//     case division
-//     init?(selectedSegmentIndex: Int) {
-//         switch selectedSegmentIndex {
-//         case 0:
-//             self = .addition
-//         case 1:
-//             self = .subtraction
-//         case 2:
-//             self = .multiplication
-//         case 3:
-//             self = .division
-//         default:
-//             return nil
-//         }
-//     }
-// }
-// @IBOutlet private weak var textField1: UITextField!
-// @IBOutlet private weak var textField2: UITextField!
-// @IBOutlet private weak var segment: UISegmentedControl!
-// @IBOutlet private weak var label: UILabel!
-// @IBAction private func button(_ sender: Any) {
-//     guard let operation = Operation(rawValue: segment.selectedSegmentIndex) else {
-//         print("segment.selectedSegmentIndex is invalid.")
-//         return
-//     }
-//     // isEmpty を使うと意図がより明確になる
-//     guard !textField1.text!.isEmpty, !textField2.text!.isEmpty else {
-//         label.text = "未入力の項目があります"
-//         return
-//     }
-//     guard let value1 = Int(textField1.text!), let value2 = Int(textField2.text!) else {
-//         label.text = "数字を入力してください"
-//         return
-//     }
-//     switch operation {
-//     case .addition:
-//         label.text = "\(value1 + value2)"
-//     case .subtraction:
-//         label.text = "\(value1 - value2)"
-//     case .multiplication:
-//         label.text = "\(value1 * value2)"
-//     case .division:
-//         guard value1 != 0 && value2 != 0 else {
-//             label.text = "割る数には0以外を入力してください"
-//             return
-//         }
-//         label.text = "\(Double(value1) / Double(value2))"
-//     }
-// }
+//enum Operation: Int {
+//    case addition
+//    case subtraction
+//    case multiplication
+//    case division
+//
+//    init?(selectedSegmentIndex: Int) {
+//        switch selectedSegmentIndex {
+//        case 0:
+//            self = .addition
+//        case 1:
+//            self = .subtraction
+//        case 2:
+//            self = .multiplication
+//        case 3:
+//            self = .division
+//        default:
+//            return nil
+//        }
+//    }
+//}
+//
+//@IBAction private func button(_ sender: Any) {
+//    guard let operation = Operation(rawValue: segmentedControl.selectedSegmentIndex) else {
+//        print("segment.selectedSegmentIndex is invalid.")
+//        return
+//    }
+//    // isEmpty を使うと意図がより明確になる
+//    guard !(textField1.text ?? "").isEmpty, !(textField2.text ?? "").isEmpty else {
+//        answerLabel.text = "未入力の項目があります"
+//        return
+//    }
+//    guard let value1 = Int(textField1.text ?? ""), let value2 = Int(textField2.text ?? "") else {
+//        answerLabel.text = "数字を入力してください"
+//        return
+//    }
+//    switch operation {
+//    case .addition:
+//        answerLabel.text = "\(value1 + value2)"
+//    case .subtraction:
+//        answerLabel.text = "\(value1 - value2)"
+//    case .multiplication:
+//        answerLabel.text = "\(value1 * value2)"
+//    case .division:
+//        guard value1 != 0 && value2 != 0 else {
+//            answerLabel.text = "割る数には0以外を入力してください"
+//            return
+//        }
+//        answerLabel.text = "\(Double(value1) / Double(value2))"
+//    }
+//}
+
+// 方法3
+// 2つの値の計算に関する責務のみ受け持つ
+//enum Operator: Int {
+//    case add
+//    case subtract
+//    case multiply
+//    case devide
+//
+//    enum Error: Swift.Error {
+//        case dividingByZero
+//    }
+//
+//    func calculate(source: CalculatingSource) throws -> Int {
+//        switch self {
+//        case .add, .subtract, .multiply:
+//            break
+//        case .devide:
+//            guard source.value2 != 0 else {
+//                throw Error.dividingByZero
+//            }
+//        }
+//        return ope(source.value1, source.value2)
+//    }
+//
+//    private var ope: (Int, Int) -> Int {
+//        switch self {
+//        case .add:
+//            return (+)
+//        case .subtract:
+//            return (-)
+//        case .multiply:
+//            return (*)
+//        case .devide:
+//            return (/)
+//        }
+//    }
+//}
+//
+//final class ViewController: UIViewController {
+//    @IBOutlet private var numTextFields: [UITextField]!
+//    @IBOutlet private weak var resultCalcLabel: UILabel!
+//    @IBOutlet private weak var operatorSegment: UISegmentedControl!
+//
+//    @IBAction func pressButton(_ sender: Any) {
+//        guard let calculatingSource = InputNumberExtractor().extract(numTextFields: numTextFields) else {
+//            resultCalcLabel.text = "各枠に数字（半角）を入力して下さい"
+//            return
+//        }
+//
+//        guard let ope = Operator(rawValue: operatorSegment.selectedSegmentIndex) else {
+//            fatalError("selectedSegmentIndex is invalid.")
+//        }
+//
+//        do {
+//            resultCalcLabel.text = String(try ope.calculate(source: calculatingSource))
+//        } catch Operator.Error.dividingByZero {
+//            resultCalcLabel.text = "割る数には0以外を入力して下さい"
+//        } catch {
+//
+//        }
+//    }
+//}
+//
+//// 計算で扱う2つの値を保持する責務のみ持つ
+//struct CalculatingSource {
+//    let value1: Int
+//    let value2: Int
+//}
+//
+//// textFieldsから2つの値を取り出すことに関する責務のみ持つ
+//struct InputNumberExtractor {
+//    func extract(numTextFields: [UITextField]) -> CalculatingSource? {
+//        let numbers = numTextFields.map { $0.text ?? "" }.compactMap { Int($0) }
+//        guard numbers.count == 2 else {
+//            return nil
+//        }
+//        return CalculatingSource(value1: numbers[0], value2: numbers[1])
+//    }
+//}
 
 // 参考
-//let nums = [self.textField1, self.textField2].map{Int($0.text ?? "") ?? 0}
+//let nums = [self.textField1, self.textField2].map{ Int($0.text ?? "") ?? 0 }
